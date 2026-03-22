1cb55528463086fed90f169ae869506fc88a874b
kartinkak76-commits <kartinkak76@gmail.com>
2026-03-22 21:35:38 +0300

    a

--- shop/views.py
+++ shop/views.py
@@ -15,7 +15,7 @@ from django.contrib import messages
 from django.http import JsonResponse
 from django.core.mail import send_mail
 from django.conf import settings
-import timedelta
+from datetime import timedelta
 from pytz import timezone
 import join
 
